cafdb6545f33d4a5f564db31e2c165e1cb874285
Harriet Holman <harriet.holman@epigenesys.org.uk>
2023-12-01 22:00:47 +0000

    Day 01 Pt 2 Solution and comments

--- day01/day01-2.rb
+++ day01/day01-2.rb
@@ -1,5 +1,5 @@
 #Take the input and make a new item in an array that is separate for each line of input
-input = File.open("./example2.txt").read.split("\n")
+input = File.open("./input.txt").read.split("\n")
 
 numbers = {
 "one" => 1,
@@ -17,28 +17,35 @@ lines_without_letters = []
 
 # For each item in the array strip out the non-digit characters and push that to the array as a combined string
 input.each_with_index do |input_numerical, index|
-    puts ""
     first_digit = 0
     last_digit = 0
     temp_string = ""
     
     input_numerical.each_char do |line|
+        # If the character is a digit, update the first_digit
         if line.match(/(\d+)/)
             first_digit = line.to_i
             break
         else
+            # Otherwise, keep adding characters to a string and compare against the hash of numbers until you get a match, then update the first_digit
             temp_string = temp_string+line
             numbers.each do |number_as_word, number_as_digit|
                 if temp_string.include? number_as_word
                     first_digit = numbers[number_as_word]
                 end
             end
+            # Stop when the first digit is found
             if first_digit > 0
             break
             end
         end
     end
 
+    # Clear the temporary string between first and last digit checks.
+    # VERY IMPORTANT!
+    temp_string = ""
+
+    # Repeat as above for the last digit, but everything's backwards
     input_numerical.reverse.each_char do |line|
         if line.match(/(\d+)/)
             last_digit = line.to_i
@@ -46,9 +53,11 @@ input.each_with_index do |input_numerical, index|
         else
             temp_string = temp_string+line
             numbers.each do |number_as_word, number_as_digit|
+                # Except this string still needs to be the right way round
                 if temp_string.reverse.include? number_as_word
                     last_digit = numbers[number_as_word]
                 end
+                puts temp_string
             end
             if last_digit > 0
             break
@@ -57,23 +66,10 @@ input.each_with_index do |input_numerical, index|
     end
 
     puts input_numerical
+    puts input_numerical.reverse
     puts "#{first_digit}#{last_digit}"
     
     lines_without_letters.push "#{first_digit}#{last_digit}".to_i
 end
 
-# puts lines_without_letters
-
 puts lines_without_letters.sum
-
-# values = []
-
-# # For each item of the new array, take the first and last character and save it to a new array as a combined integer
-# lines_without_letters.each do |line|
-#     firstNo = line.split("").first
-#     lastNo = line.split("").last
-#     values.push("#{firstNo}#{lastNo}".to_i)
-# end
-
-# # Add up all the items in the new array
-# #puts values.sum
